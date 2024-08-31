ff3935629c21547db94853575dd24a6e500d5cfc
Lance Haynie <lhaynie@santaclarautah.gov>
2024-08-31 16:39:10 -0600

    DEV-285

--- config/sidebar.php
+++ config/sidebar.php
@@ -2,16 +2,50 @@
 return [
     'menu' => [
         [
+            'icon' => 'fa fa-desktop',
+            'title' => 'Mobile Data Command',
+            'url' => '/comingsoon',
+            'route-name' => 'mdc'
+        ],
+        [
             'icon' => 'fa fa-fire',
-            'title' => 'Fire Command',
-            'url' => '/FireCommand/calls',
+            'title' => 'Incident Command',
+            'url' => '/ic/calls',
+            'route-name' => 'fc-calls'
+        ],
+        [
+            'icon' => 'fa fa-building',
+            'title' => 'Incident Planning',
+            'url' => '/comingsoon',
+            'route-name' => 'preplan'
+        ],
+        [
+            'icon' => 'fa fa-plane',
+            'title' => 'DroneCAD',
+            'url' => '/comingsoon',
+            'route-name' => 'dronecad'
+        ],
+        [
+            'icon' => 'fa fa-gear',
+            'title' => 'Spillman Automation',
+            'url' => '/comingsoon',
             'caret' => true,
             'sub_menu' => [
                 [
-                    'url' => '/FireCommand/calls',
-                    'title' => 'Active Calls',
-                    'route-name' => 'fc-calls',
-                ]
+                    'url' => '/comingsoon',
+                    'title' => 'Agency Setup',
+                    'route-name' => 'sa-agency',
+                ],
+                [
+                    'url' => '/comingsoon',
+                    'title' => 'Natures',
+                    'route-name' => 'sa-nature',
+                ],
+                [
+                    'url' => '/comingsoon',
+                    'title' => 'City Setup',
+                    'route-name' => 'sa-city',
+                ],
             ],
         ],
     ],
