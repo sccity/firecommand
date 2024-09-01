564fcfe2882e3af64e479055879e67e52f2faaa3
Lance Haynie <lhaynie@santaclarautah.gov>
2024-09-01 16:14:01 -0600

    DEV-285

--- resources/views/pages/ic/command.blade.php
+++ resources/views/pages/ic/command.blade.php
@@ -50,24 +50,14 @@
 @section('content')
 
     <div class="container" data-call-id="{{ $call_id }}">
-        <!-- BEGIN panel -->
-        <div class="panel panel-inverse" data-sortable-id="ui-widget-18">
-            <div class="panel-heading">
-                <h4 class="panel-title">Incident Details</h4>
-                <div class="panel-heading-btn">
-                    <a href="javascript:;" class="btn btn-xs btn-icon btn-default" data-toggle="panel-expand"><i
-                            class="fa fa-expand"></i></a>
-                    <a href="javascript:;" class="btn btn-xs btn-icon btn-warning" data-toggle="panel-collapse"><i
-                            class="fa fa-minus"></i></a>
-                </div>
-            </div>
+
             <div class="panel-body bg-gray-800 text-white">
                 <h1 style="text-transform: uppercase;">{{ $callnum }} {{ $nature }} - {{ $address }} -
                     {{ $city }}/{{ $zone }}</h1>
                 <!-- BEGIN row -->
                 <div class="row" style="margin-left: 0; margin-right: 0;">
                     <!-- BEGIN col-9 for the map -->
-                    <div id="map" class="col-xl-9 col-md-12" style="height: 300px;"></div>
+                    <div id="map" class="col-xl-9 col-md-12" style="height: 250px;"></div>
                     <!-- END col-9 -->
 
                     <!-- BEGIN col-3 for the timer -->
@@ -99,11 +89,6 @@
                 </div>
                 <!-- END row -->
             </div>
-            <div class="hljs-wrapper">
-                <pre><code class="html" data-url="/assets/data/ui-widget-boxes/code-18.json"></code></pre>
-            </div>
-        </div>
-        <!-- END panel -->
         <!-- BEGIN panel -->
         <div class="panel panel-inverse" data-sortable-id="ui-widget-18">
             <div class="panel-heading">
